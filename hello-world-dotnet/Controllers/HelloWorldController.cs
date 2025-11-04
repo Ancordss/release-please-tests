60effa775ff912dd035f176b19821a96085d8ad5
Ancordss <maradiaga.l.james@gmail.com>
2025-11-04 15:44:44 -0600

    fix: corregir bug en el cálculo del interés compuesto

--- hello-world-dotnet/Controllers/HelloWorldController.cs
+++ hello-world-dotnet/Controllers/HelloWorldController.cs
@@ -12,7 +12,7 @@ namespace hello_world_dotnet.Controllers
         {
             return new()
             {
-                Message = "Automate all the things! bumping version 0.0.1 ",
+                Message = "Automate all the things! bumping version 0.0.1  ",
                 Timestamp = DateTime.Now
             };
         }
